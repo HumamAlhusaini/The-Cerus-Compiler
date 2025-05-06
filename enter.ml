6e13bdadcf06df74d8954a170e99e7f268304ae0
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-05-06 13:38:38 -0500

    properly integrated pre parser and print ast

--- enter.ml
+++ enter.ml
@@ -1,32 +1,37 @@
 open Lexer
 open Parser
+open Pre_parser
 open Cabs
 open Sedlexing
 open Pprint
 open MenhirLib.Convert
+
 
 let print_tokens_from_file filename =
   let chan = open_in filename in
   let lexbuf = Sedlexing.Utf8.from_channel chan in
   let rec loop () =
     let token = token lexbuf in
-    match token with
+      let c_token = Pprint.convert_token token in 
+    match c_token with
     | Parser.EOF _ -> print_endline "End of file."
     | t ->
         print_endline (string_of_token t);
         loop ()
   in
   loop ()
-let print_ast_from_file filename = 
-    let chan = open_in filename in
-    let lexbuf = Sedlexing.Utf8.from_channel chan in
-    let supplier = Sedlexing.with_tokenizer token lexbuf in
-    let parser = MenhirLib.Convert.Simplified.traditional2revised Parser.program in
-    let ast = parser supplier in
-    print_endline (Pprint.string_of_something ast)
+
+let print_ast_from_file filename =
+  let chan = open_in filename in
+  let lexbuf = Sedlexing.Utf8.from_channel chan in
+  let supplier = Sedlexing.with_tokenizer token lexbuf in
+  let parser = MenhirLib.Convert.Simplified.traditional2revised Pre_parser.program in
+  let ast = parser supplier in
+  print_endline (string_of_program ast)
 
 let () =
   if Array.length Sys.argv <> 2 then
     Printf.eprintf "Usage: %s <filename>\n" Sys.argv.(0)
   else
     print_tokens_from_file Sys.argv.(1);
+    print_ast_from_file Sys.argv.(1);
